1160d2a0469078b0e3dc6a0386feede448411d2a
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-06-13 10:18:18 +0530

    completed resolv func

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -7,29 +7,26 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func ResolveURL(c *fiber.Ctx) error{
+func ResolveURL(c *fiber.Ctx) error {
 
 	url := c.Params("url")
-	r := database.CreateClient(0)
+	r := database.CreateClient(0) //mapping of shorten url and real url
 	defer r.Close()
 
-	r.Get(database.Ctx, url).Result()
-	if err == redis.Nil{
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error":"short not found in the database",
+			"error": "short not found in the database",
 		})
 	} else if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error":"cannot connect to database",
-
+			"error": "cannot connect to database",
 		})
 	}
-	rInr := database.CreateClient(1)
+	rInr := database.CreateClient(1) //rate limiting on IP
 	defer rInr.Close()
 
 	_ = rInr.Incr(database.Ctx, "counter")
 
 	return c.Redirect(value, 301)
-	}
-
 }
